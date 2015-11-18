f968836a2960b3f5a0f52e74a3a5dc7daf2cb46e
isimicic <ivan.simicic92@gmail.com>
2015-11-18 23:05:56 +0100

    added remote connection to isimicic.com for mysql databse and updated requirements.txt file

--- ticketReservation/settings.py
+++ ticketReservation/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ticketReservation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'isimicic_DjangoTest',
+        'USER': 'isimicic_django',
+        'PASSWORD': 'django',
+        'HOST': 'isimicic.com',
+        'PORT': '',
     }
 }
 

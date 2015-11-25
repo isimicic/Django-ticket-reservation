5d5c8caa464a4748d73aadc7cceab1d34ad984bb
isimicic <ivan.simicic92@gmail.com>
2015-11-25 20:13:27 +0100

    remove some fields from account/model

--- ticketReservation/apps/account/models.py
+++ ticketReservation/apps/account/models.py
@@ -4,13 +4,11 @@ from django.core.validators import RegexValidator
 
 
 class UserProfile(models.Model):
+    """ User Profile Model """
     user = models.OneToOneField(User, primary_key=True)
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed")
     number = models.CharField(max_length=15, validators=[phone_regex])
-    email = models.EmailField(max_length=70)
 
 User.profile = property(lambda u: UserProfile.objects.get_orcreate(user=u)[0])

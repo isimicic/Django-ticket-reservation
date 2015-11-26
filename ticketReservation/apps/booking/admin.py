05aab65023a8e434f07fe21e94ebee40774c4643
isimicic <ivan.simicic92@gmail.com>
2015-11-26 17:52:54 +0100

    booking app->changed model and admin

--- ticketReservation/apps/booking/admin.py
+++ ticketReservation/apps/booking/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(Movie)
-admin.site.register(City)
-admin.site.register(Cinema)
+
 admin.site.register(Auditorium)
 admin.site.register(Seat)
 admin.site.register(ReservationType)
 admin.site.register(Reservation)
 admin.site.register(Screening)
 admin.site.register(SeatReserved)
+
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'auditorium_number', 'city')
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- ticketReservation/apps/booking/models.py
+++ ticketReservation/apps/booking/models.py
@@ -8,30 +8,32 @@ class Movie(models.Model):
     director = models.CharField(max_length=255)
     cast = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.CharField(max_length=5)
+    CHOICES = [(i, i) for i in range(50, 150, 1)]
+    duration = models.IntegerField(choices=CHOICES)
 
     def __unicode__(self):
-        return "{0} {1} {2} {3} {4} {5}".format(
-            self, self.title, self.director, self.cast,
+        return "{0} {1} {2} {3} {4}".format(
+            self.title, self.director, self.cast,
             self.description, self.duration)
 
 
 class City(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
-        return "{0} {1}".format(self, self.name)
+    def __str__(self):
+        return "{0}".format(self.name)
 
 
 class Cinema(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    auditorium_number = models.CharField(max_length=100)
+    CHOICES = [(i, i) for i in range(1, 21, 1)]
+    auditorium_number = models.IntegerField(choices=CHOICES)
     city = models.ForeignKey(City)
 
     def __unicode__(self):
-        return "{0} {1} {2} {3}".format(
-            self, self.name, self.address, self.auditorium_number)
+        return "{0} {1} {2}".format(
+            self.name, self.address, self.auditorium_number)
 
 
 class Auditorium(models.Model):
@@ -40,36 +42,37 @@ class Auditorium(models.Model):
     cinema = models.ForeignKey(Cinema)
 
     def __unicode__(self):
-        return "{0} {1} {2}".format(self, self.name, self.seats_number)
+        return "{0} {1}".format(self.name, self.seats_number)
 
 
 class Seat(models.Model):
     row = models.CharField(max_length=2)
-    number = models.CharField(max_length=2)
+    CHOICES = [(i, i + 1) for i in range(20)]
+    number = models.IntegerField(choices=CHOICES)
     auditorium = models.ForeignKey(Auditorium)
 
     def __unicode__(self):
-        return "{0} {1} {2}".format(self, self.row, self.number)
+        return "{0} {1}".format(self.row, self.number)
 
 
 class ReservationType(models.Model):
     type = models.CharField(max_length=255)
 
     def __unicode__(self):
-        return "{0} {1}".format(self, self.type)
+        return "{0}".format(self.type)
 
 
 class Reservation(models.Model):
     reservation_date = models.DateTimeField(default=datetime.now)
-    reserved = models.BooleanField(default=False)
-    paid = models.BooleanField(default=False)
+    reserved = models.BooleanField(default=True)
+    paid = models.BooleanField(default=True)
     active = models.BooleanField(default=True)
     reservation_type = models.ForeignKey(ReservationType)
     user = models.ForeignKey(User)
 
     def __unicode__(self):
-        return "{0} {1} {2} {3} {4}".format(
-            self, self.reservation_date, self.reserved, self.paid, self.active)
+        return "{0} {1} {2} {3}".format(
+            self.reservation_date, self.reserved, self.paid, self.active)
 
 
 class Screening(models.Model):
@@ -79,7 +82,7 @@ class Screening(models.Model):
     reserved_seats = models.ManyToManyField(Seat, through='SeatReserved')
 
     def __unicode__(self):
-        return "{0} {1}".format(self, self.screening_start)
+        return "{0}".format(self.screening_start)
 
 
 class SeatReserved(models.Model):

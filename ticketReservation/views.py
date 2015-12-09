7c58e53cecf66287fde7f6ff99979c91bbdf963e
isimicic <ivan.simicic92@gmail.com>
2015-12-09 14:57:09 +0100

    changed navigation and static site dir

--- ticketReservation/views.py
+++ ticketReservation/views.py
@@ -4,12 +4,12 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     """ View that will redirect us if we click logout """
-    template_name = 'index.html'
+    template_name = 'staticSites/index.html'
 
 
 class AboutUsView(TemplateView):
-    template_name = "aboutUs.html"
+    template_name = "staticSites/aboutUs.html"
 
 
 class ContactView(TemplateView):
-    template_name = "contact.html"
+    template_name = "staticSites/contact.html"

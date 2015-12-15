26cf1c977cf07a439a799aa607443c19b378e09d
isimicic <ivan.simicic92@gmail.com>
2015-12-16 00:23:01 +0100

    in single movie page added your vote

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView
 from django.contrib.sites.models import Site
+from django.shortcuts import get_object_or_404
 from .models import Movie
+from rating.models import Rating
 
 
 class MovieView(DetailView):
@@ -10,5 +12,7 @@ class MovieView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(MovieView, self).get_context_data(**kwargs)
+        context['vote'] = Rating.objects.get(user=self.request.user,
+                                             movie=context['object'])
         context['site'] = Site.objects.get_current()
         return context
